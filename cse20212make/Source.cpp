89f58241682b726e8016533e13c58b7ad3b0db76
Alex Wu <alexwu@ubuntu.(none)>
2014-03-27 18:43:09 -0400

    Made it work with giggle

---
+++ cse20212make/Source.cpp
@@ -0,0 +1,31 @@
+/* Pokemon Project
+Alex Hansen
+Source.cpp
+
+Testing class used to test implementations in parts
+*/
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "BoardPiece.h"
+#include "Location.h"
+#include "Pokemon.h"
+#include <fstream>
+#include <vector>
+#include "Game.h"
+
+using namespace std;
+
+int main(int argc, char* args[])
+{
+	Game one = Game();
+	one.test();
+
+	while (1)
+	{
+
+	}
+}
